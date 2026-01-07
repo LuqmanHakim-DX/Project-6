4b21311e28871ee06fbf4ca83551c52b896720d4
LuqmanHakim-DX <misterkim147@gmail.com>
2026-01-07 09:29:57 +0800

    - Added xml layouts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation("androidx.navigation:navigation-compose:2.7.0")
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
